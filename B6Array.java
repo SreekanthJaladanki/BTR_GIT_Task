1014510a0cc0799bf681cef7fc4f83cbe60cce12
poojitha <poojitha>
2023-11-20 18:30:01 +0000

    commit

---
+++ B6Array.java
@@ -0,0 +1,22 @@
+public class B6Array 
+{
+	public static void main(String[] args)
+	{
+		int aryy[] = { 99,88,77,66,55 };	 // 0,1,2,3,4 array count the value starting with 0
+
+		int ary[] = new int[5];
+		ary[0] = 5;
+		ary[1] = 15;
+		ary[2] = 25;
+		ary[3] = 35;
+		ary[4] = 45;
+
+	for(int i=0; i<5; i++)
+
+		System.out.println(ary[i]); 
+
+		System.out.println(aryy[3]);		// if change the value 0 or 1 or 2 or 3 or 4 you get different OP
+
+
+	}
+}
